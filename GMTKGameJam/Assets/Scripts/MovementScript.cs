ee90d810204578e6e7c2e6a8a4a9ae5ba10fb8c2
Varun A. Vyas <varunvyas55@gmail.com>
2023-07-08 11:14:04 +0530

    Added movement to Character

---
+++ GMTKGameJam/Assets/Scripts/MovementScript.cs
@@ -0,0 +1,38 @@
+using SODefinitions;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    [SerializeField] float tolerance;
+    [SerializeField] CharacterSO stats;
+    private Vector2 movement;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        rb.isKinematic = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxis("Horizontal");
+        movement.y = Input.GetAxis("Vertical");
+        movement.Normalize();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * (stats.MoveSpeed * Time.fixedDeltaTime));
+
+        if (movement.sqrMagnitude > tolerance)
+        {
+            float moveAngle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg - 90f;
+            transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, 0, moveAngle), 0.15f);
+        }
+    }
+}
